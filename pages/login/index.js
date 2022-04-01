e4b6cd2bb89cfbcb804cfd9dd2f12ed5793893ad
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-04-01 18:26:59 +0200

    Login as a component

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,74 +1,16 @@
-import { useState } from 'react';
-import { useRouter } from 'next/router';
-import Link from 'next/link';
-import { useLogin } from '../../hooks/useLogin';
-import styles from '../../styles/SignUpIn.module.css';
+
 import Navbar from '../../components/nav/Navbar';
+import Login from '../../components/authentication/Login';
 
-export default function Login() {
-	const [email, setEmail] = useState('');
-	const [password, setPassword] = useState('');
-	const { error, login } = useLogin();
-
-	const router = useRouter();
-
-	const handleOnClickHome = e => {
-		e.preventDefault();
-		router.push('/');
-	};
-
-	const handleSubmit = e => {
-		e.preventDefault();
-		login(email, password);
-		router.push('/');
-	};
+export default function LoginPage() {
+	
 
 	return (
-		<div className={styles.container}>
+		<div >
 			<Navbar />
-			<div className={styles.form__position}>
-				<div className={styles.form__container}>
-					<div className={styles.form__goBack} onClick={handleOnClickHome}>
-						<Link href='/'>
-							<a>X</a>
-						</Link>
-					</div>
-
-					<h2 className={styles.form__title}>Aanmelden</h2>
-					<form onSubmit={handleSubmit} className={styles.form}>
-						<label className={styles.form__label}>
-							<span className={styles.form__labelTitle}>E-mailadres</span>
-							<input
-								required
-								type='email'
-								onChange={e => setEmail(e.target.value)}
-								value={email}
-								className={styles.form__labelInput}
-							/>
-						</label>
-						<label className={styles.form__label}>
-							<span className={styles.form__labelTitle}>
-								Wachtwoord
-								<Link href='/forgot-password'>
-									<a className={styles.form__forgotpassword}>
-										Wachtwoord vergeten?
-									</a>
-								</Link>
-							</span>
-							<input
-								required
-								type='password'
-								onChange={e => setPassword(e.target.value)}
-								value={password}
-								className={styles.form__labelInput}
-							/>
-						</label>
-
-						<button className={styles.form__button}>Bevestig</button>
-						{error && <p>{error}</p>}
-					</form>
-				</div>
-			</div>
+			<Login />
+			
+			
 		</div>
 	);
 }
