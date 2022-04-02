38f0bc765bf1899bacaa585e32cbb714b7fa51e1
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-04-02 06:29:17 +0200

    modal component (3)

--- components/modal/modal.js
+++ components/modal/modal.js
@@ -5,7 +5,7 @@ import styles from '../../styles/Modal.module.css';
 
 const Modal = ({ show, onClose, children, title }) => {
 	const [isBrowser, setIsBrowser] = useState(false);
-
+  console.log(isBrowser)
 	useEffect(() => {setIsBrowser(true)},[]);
 
   const handleClose = (e) => {
