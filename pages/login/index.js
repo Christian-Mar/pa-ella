319b8762b9819b2905d2f95554de5304259812d0
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-04-05 22:07:14 +0200

    Profile image in Nav 2

--- pages/login/index.js
+++ pages/login/index.js
@@ -1,11 +1,9 @@
-
+import Head from 'next/head';
 import Navbar from '../../components/nav/Navbar';
 import Banner from '../../components/banner/Banner';
 import Login from '../../components/authentication/Login';
 
 export default function LoginPage() {
-	
-
 	return (
 		<div>
 			<Head>
