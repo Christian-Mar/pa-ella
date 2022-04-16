e7871d461159df45b2b2e67086f0203cbad5b4e1
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-04-16 09:08:42 +0200

    start create recipe

--- pages/create-recipe/index.js
+++ pages/create-recipe/index.js
@@ -1,17 +1,54 @@
+import { useState } from 'react';
 import Head from 'next/head';
 import Navbar from '../../components/nav/Navbar';
+import { db } from '../../firebase/config';
+import { collection, addDoc, Timestamp } from 'firebase/firestore';
 import styles from '../../styles/CreateRecipe.module.css';
 
 const CreateRecipe = () => {
+	const [title, setTitle] = useState('');
+	const [description, setDescription] = useState('');
+	
+
+	const handleSubmit = async e => {
+		e.preventDefault();
+		try {
+			await addDoc(collection(db, 'recipes'), {
+				title: title,
+				description: description,
+				created: Timestamp.now(),
+			});
+			setTitle('');
+			setDescription('');
+		} catch (err) {
+			alert(err);
+		}
+	};
+
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>pa'ella create</title>
+				<title>pa'el-la create</title>
 				<meta name='description' content='The recipe website' />
 				<link rel='icon' href='/images/favicon.ico' />
 			</Head>
 			<Navbar />
 			<h1>Create Recipe</h1>
+			<form onSubmit={handleSubmit} name='createRecipe'>
+				<input
+					type='text'
+					name='title'
+					onChange={e => setTitle(e.target.value)}
+					value={title}
+					placeholder='new recipe'
+				/>
+				<textarea
+					onChange={e => setDescription(e.target.value)}
+					placeholder='description'
+					value={description}
+				></textarea>
+				<button type='submit'>Submit</button>
+			</form>
 		</div>
 	);
 };
