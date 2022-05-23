e5e5f8e8a725fbcb2964ed3883b24cd83b94c167
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-05-23 16:13:01 +0200

    styling About page

--- pages/about/index.js
+++ pages/about/index.js
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import Navbar from '../../components/nav/Navbar';
 import Footer from '../../components/footer/Footer';
+import Banner from '../../components/banner/Banner';
+import styles from '../../styles/About.module.css';
 
 const About = () => {
 	return (
@@ -11,22 +13,21 @@ const About = () => {
 				<link rel='icon' href='/images/favicon.ico' />
 			</Head>
 			<Navbar />
-			<div>
+			<Banner imgUrl='/images/ellas.jpg' />
+			<div className={styles.container}>
 				<h1>Over pa'el-la</h1>
+				<p><b>Pa'el-la</b> is het antwoord op: "Wat eten we vandaag?".</p>
 				<p>
-					Pa'el-la is het antwoord op: "Wat eten we vandaag?".
-				</p>
-				<p>
-					Waarom de naam pa'el-la? Wie de oorsprong van dit Spaanse gerecht
+					Waarom de naam <b>pa'el-la</b>? Wie de oorsprong van dit Spaanse gerecht
 					probeert te behappen, verdwaalt in tal van verhalen en legenden.
-					Taalkundigen houden het op op de pan die voor de paëlla gebruikt wordt
-					en in het Latijn 'patella' genoemd wordt. Anderen verwijzen naar het
+					Taalkundigen houden het op de pan die voor de paella gebruikt wordt en
+					in het Latijn 'patella' genoemd wordt. Anderen verwijzen naar het
 					Arabische 'baquiyah', naar de origine van de rijst die uit
 					Noord-Afrika zou komen.
 				</p>
 				<p>
 					Een heroïsch verhaal over het ontstaan van de beroemde paella vertelt
-					dat tijdens de Onafhankelijkheidsoorlog een Franse generaal zeer onder
+					dat tijdens de Spaanse Onafhankelijkheidsoorlog een Franse generaal zeer onder
 					de indruk was van de bereiding van paella door een vrouw, zodat hij
 					met haar een deal sloot: voor elk nieuw bord rijst zou hij een Spaanse
 					gevangene bevrijden. Met dit voor ogen liet de vrouw haar fantasie de
@@ -35,18 +36,23 @@ const About = () => {
 				</p>
 
 				<p>
-					Wij houden het op de - wellicht meest romantische - legende. Zo zou
-					een vrouw die haar zieke moeder ging bezoeken een dag van huis geweest
-					zijn. Tegen de vele Spaanse tradities van die tijd in, besloot haar
-					man voor haar thuiskomst een gerecht in elkaar te knutselen met de restjes van
-					de dagen ervoor. Het was een heerlijk gerecht en al vlug deed het de
-					ronde in het dorp dat hij voor haar - <i>para ella </i>- had gekookt.
-					In het lokale dialect klonk dit als <i>pa'ella</i>. Even verder in de
-					tijd, maken we er hier pa'el-la van, voor hem - <i>pa'el</i> -, voor
-					haar - <i>pa'ella</i> - en iedereen die zich tussen die twee voelt.
-					Voor iedereen, dus. Recepten zijn zoveel meer dan de chemie van wat
-					ingrediënten, omdat je altijd voor iemand kookt, ook al is die
-					speciale iemand je eigenste zelf.
+					Wij houden het op de volgende - wellicht meest romantische - legende. Zo zou
+					een vrouw, die haar zieke moeder ging bezoeken, een dag van huis
+					geweest zijn. Tegen de vele Spaanse tradities van die tijd in, besloot
+					haar man voor haar thuiskomst een gerecht in elkaar te knutselen met
+					de restjes van de dagen ervoor. Het was een heerlijk gerecht! Al vlug
+					deed het de ronde in het dorp dat hij voor haar - <i>para ella</i> -
+					had gekookt. In het lokale dialect klonk dit als <i>pa'ella</i>. Niet
+					meteen het gerecht zelf gingen over de tongen, dan wel het feit dat
+					hij het voor haar had gedaan. Tot op de dag van vandaag worden de
+					meeste paella's nog steeds door mannen gemaakt.
+				</p>
+				<p>
+					Even verder in de tijd, maken we er hier <b>pa'el-la</b> van, voor hem
+					- <i>pa'el</i> -, voor haar - <i>pa'ella</i> - en het streepje voor
+					iedereen die zich tussen die twee voelt. Voor iedereen, dus. Recepten
+					zijn zoveel meer dan de chemie van wat ingrediënten, omdat je altijd
+					voor iemand kookt, ook al is die speciale iemand je eigenste zelf.
 				</p>
 			</div>
 			<Footer />
