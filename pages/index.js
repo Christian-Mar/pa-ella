098fbc7ac0e56bf4be95aeca20fd9eab7d61f647
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-05-25 10:16:45 +0200

    SSR again

--- pages/index.js
+++ pages/index.js
@@ -21,7 +21,7 @@ const breakPoints = [
 	{ width: 1400, itemsToShow: 6 },
 ];
 
-export const getStaticProps = async () => {
+export const getServerSideProps = async () => {
 	
 	const querySnapshot = await getDocs(collection(db,'recipes'));
 	let recipes= [];
