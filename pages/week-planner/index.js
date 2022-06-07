bbc7e3f4c0741a0e812941a69e1b8165f33d39c5
Christian <71664957+Christian-Mar@users.noreply.github.com>
2022-06-07 06:11:46 +0200

    start dnd weekplanner

--- pages/week-planner/index.js
+++ pages/week-planner/index.js
@@ -7,7 +7,8 @@ import { useAuthContext } from '../../hooks/useAuthContext';
 import styles from '../../styles/Weekplanner.module.css';
 import { db } from '../../firebase/config';
 import { getDocs, collection } from 'firebase/firestore';
-
+import { DragDropContext, Draggable, Droppable } from 'react-beautiful-dnd';
+import { v4 as uuidv4 } from 'uuid';
 
 export const getServerSideProps = async () => {
 	const querySnapshot = await getDocs(collection(db, 'recipes'));
@@ -22,12 +23,20 @@ export const getServerSideProps = async () => {
 	};
 };
 
-const WeekPlanner = ({recipes}) => {
-
-	//const [filters, setFilters] = useState({});	
+const WeekPlanner = ({ recipes }) => {
+	//const [filters, setFilters] = useState({});
+	const [positions, setPositions] = useState();
 	const { user } = useAuthContext();
 	const recipesReadable = JSON.parse(recipes);
 	const recipesData = Array.from(recipesReadable);
+	
+	const onDragEnd = result => {
+		if(!result.destination) return;
+		const items = Array.from(positions);
+		const [reorderedItem] = items.splice(result.source.index, 1);
+		items.splice(result.destination.index, 0, reorderedItem);
+		setPositions(items);
+	};
 
 	return (
 		<div>
@@ -38,29 +47,57 @@ const WeekPlanner = ({recipes}) => {
 			</Head>
 			<div className={styles.container}>
 				<Navbar />
-				<h1>Weekplanner</h1>
-				<ul className={styles.recipe__list}>{recipesData?.map(
-					recipe =>
-						recipe.category === 'dessert' && (
-							<li key={recipe.id} className={styles.recipe__listitems}>
-								
-									<div>
-										<h3 className={styles.recipe__title}>{recipe.title}</h3>
-										<h4 className={styles.recipe__category}>
-											{recipes.methodTime}
-										</h4>
-										<Image
-											src={recipe.image}
-											alt='Dish'
-											width={200}
-											height={150}
-											objectFit='cover'
-										></Image>
-									</div>
-								
-							</li>
-						)
-				)}</ul>
+				<h1>Weekplanner</h1><div className={styles.planning__container}>
+						The planning will come here
+					</div>
+				<DragDropContext onDragEnd={onDragEnd}>
+					
+				
+						<Droppable
+							//key={uuidv4()}
+							doppableId='recipes'
+							className={styles.recipe__listitems}
+						>
+							{provided => (
+								<ul
+									className={styles.recipe__list}
+									{...provided.droppableProps}
+									ref={provided.innerRef}
+								>
+									{recipesData?.map(
+										(recipe, index) =>
+											recipe.category === 'dessert' && (
+												<Draggable key={recipe.id} draggableId={recipe.id} index={index}>
+												{(provided) => (
+													<li {...provided.draggableProps} {...provided.dragHandleProps} ref={provided.innerRef}
+													
+													className={styles.recipe__listitems}
+												>
+													<div>
+														<h3 className={styles.recipe__title}>
+															{recipe.title}
+														</h3>
+														<h4 className={styles.recipe__category}>
+															{recipes.methodTime}
+														</h4>
+														<Image
+															src={recipe.image}
+															alt='Dish'
+															width={200}
+															height={150}
+															objectFit='cover'
+														></Image>
+													</div>
+												</li>)}
+												</Draggable>
+											)
+									)}
+									{provided.placeholder}
+								</ul>
+							)}
+						</Droppable>
+					
+				</DragDropContext>
 			</div>
 			<Footer />
 		</div>
